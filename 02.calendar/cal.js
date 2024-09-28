d629fb3f2d30df7f173816db0104ba20d41c1048
zecky1120 <takahide1120@gmail.com>
2024-09-28 20:25:21 +0900

    fix: 変数の名称変更(d => day), 曜日の配列を削除,dayjsを2行で評価したものを1つにした

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -11,19 +11,21 @@ dayjs.extend(timezone);
 dayjs.tz.setDefault("Asia/Tokyo");
 
 const argv = minimist(process.argv.slice(2));
-const year = argv.y ?? dayjs().year();
-const month = argv.m ?? dayjs().month() + 1;
+const today = dayjs();
+const year = argv.y ?? today.year();
+const month = argv.m ?? today.month() + 1;
+console.log(month);
 const endOfMonth = dayjs(`${year}-${month}`).endOf("month").daysInMonth();
 const dayNumber = dayjs(`${year}-${month}`).day();
-const weekDay = ["日", "月", "火", "水", "木", "金", "土"];
+// const weekDay = ["日", "月", "火", "水", "木", "金", "土"];
 
-console.log(`     ${month}月 ${year}     `);
-console.log(weekDay.join(" "));
+console.log(`      ${month}月 ${year}     `);
+console.log("日 月 火 水 木 金 土");
 process.stdout.write("   ".repeat(dayNumber));
-for (let d = 1; d <= endOfMonth; d++) {
-  let currentDay = dayjs(`${year}-${month}-${d}`);
+for (let day = 1; day <= endOfMonth; day++) {
+  let currentDay = dayjs(`${year}-${month}-${day}`);
   let dayOfWeek = currentDay.day();
-  process.stdout.write(String(d).padStart(2));
+  process.stdout.write(String(day).padStart(2));
   process.stdout.write(" ");
   if (dayOfWeek === 6) {
     process.stdout.write("\n");
