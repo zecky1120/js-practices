61e5c11d3477bad60ebd9df4e7ca1aabc0e75d4a
zecky1120 <takahide1120@gmail.com>
2024-09-28 20:32:43 +0900

    fix: 不要な箇所を削除

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -2,22 +2,13 @@
 
 import minimist from "minimist";
 import dayjs from "dayjs";
-import "dayjs/locale/ja.js";
-import utc from "dayjs/plugin/utc.js";
-import timezone from "dayjs/plugin/timezone.js";
-dayjs.locale("ja");
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.tz.setDefault("Asia/Tokyo");
 
 const argv = minimist(process.argv.slice(2));
 const today = dayjs();
 const year = argv.y ?? today.year();
 const month = argv.m ?? today.month() + 1;
-console.log(month);
 const endOfMonth = dayjs(`${year}-${month}`).endOf("month").daysInMonth();
 const dayNumber = dayjs(`${year}-${month}`).day();
-// const weekDay = ["日", "月", "火", "水", "木", "金", "土"];
 
 console.log(`      ${month}月 ${year}     `);
 console.log("日 月 火 水 木 金 土");
