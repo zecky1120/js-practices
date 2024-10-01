1e24e930350eec4ff65f6c502ca6b7173918dcb7
zecky1120 <takahide1120@gmail.com>
2024-10-01 21:07:55 +0900

    fix:土曜日のあとにスペースを無くす、改行のロジック変更、ループごとにdayjsを生成するのを無くす

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -7,19 +7,21 @@ const argv = minimist(process.argv.slice(2));
 const today = dayjs();
 const year = argv.y ?? today.year();
 const month = argv.m ?? today.month() + 1;
-const endOfMonth = dayjs(`${year}-${month}`).endOf("month").daysInMonth();
-const dayNumber = dayjs(`${year}-${month}`).day();
+const firstDay = dayjs(`${year}-${month}-1`);
+const lastDay = firstDay.daysInMonth();
+const space = firstDay.day();
+let currentDate = firstDay;
 
 console.log(`      ${month}月 ${year}     `);
 console.log("日 月 火 水 木 金 土");
-process.stdout.write("   ".repeat(dayNumber));
-for (let day = 1; day <= endOfMonth; day++) {
-  let currentDay = dayjs(`${year}-${month}-${day}`);
-  let dayOfWeek = currentDay.day();
-  process.stdout.write(String(day).padStart(2));
-  process.stdout.write(" ");
-  if (dayOfWeek === 6) {
+process.stdout.write("   ".repeat(space));
+for (let date = 1; date <= lastDay; date++) {
+  process.stdout.write(String(date).padStart(2));
+  if ((space + date) % 7 === 0) {
     process.stdout.write("\n");
+  } else if (date < lastDay) {
+    process.stdout.write(" ");
   }
+  currentDate = currentDate.add(1, "day");
 }
 process.stdout.write("\n");
