557f12a241226ecf1c51cdb437a5d5993345c22a
zecky1120 <takahide1120@gmail.com>
2024-11-12 22:29:38 +0900

    fix: startDateをfirstDateに変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -6,13 +6,13 @@ const argv = minimist(process.argv.slice(2));
 const now = new Date();
 const year = argv.y ?? now.getFullYear();
 const month = argv.m ?? now.getMonth() + 1;
-const startDate = new Date(year, month - 1, 1);
+const firstDate = new Date(year, month - 1, 1);
 const lastDate = new Date(year, month, 0);
 
 console.log(`      ${month}月 ${year}     `);
 console.log("日 月 火 水 木 金 土");
-process.stdout.write("   ".repeat(startDate.getDay()));
-for (let date = startDate; date <= lastDate; date.setDate(date.getDate() + 1)) {
+process.stdout.write("   ".repeat(firstDate.getDay()));
+for (let date = firstDate; date <= lastDate; date.setDate(date.getDate() + 1)) {
   process.stdout.write(String(date.getDate()).padStart(2));
   if (date.getDay() === 6 || date.getDate() === lastDate.getDate()) {
     process.stdout.write("\n");
