385a16b9991945c9d148c718db092076b21f8745
zecky1120 <takahide1120@gmail.com>
2025-03-09 20:53:36 +0900

    fix: ダブルクォーテーションに変更

--- 03.asynchronous/async_await/success.js
+++ 03.asynchronous/async_await/success.js
@@ -4,11 +4,12 @@ import sqlite3 from "sqlite3";
 import { runSql, showSql, closeSql } from "../sqlUtils.js";
 
 const db = new sqlite3.Database(":memory:");
-const createTableSql = `CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)`;
-const insertSql = `INSERT INTO books (title) VALUES (?)`;
-const bookTitle = `JavaScript Primer 迷わないための入門`;
-const selectSql = `SELECT * FROM books`;
-const dropTableSql = `DROP TABLE books`;
+const createTableSql =
+  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)";
+const insertSql = "INSERT INTO books (title) VALUES (?)";
+const bookTitle = "JavaScript Primer 迷わないための入門";
+const selectSql = "SELECT * FROM books";
+const dropTableSql = "DROP TABLE books";
 
 await runSql(db, createTableSql);
 console.log("テーブルが作成されました");
--- 03.asynchronous/promise/error.js
+++ 03.asynchronous/promise/error.js
@@ -4,11 +4,12 @@ import sqlite3 from "sqlite3";
 import { runSql, showSql, closeSql } from "../sqlUtils.js";
 
 const db = new sqlite3.Database(":memory:");
-const createTableSql = `CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)`;
-const insertSql = `INSERT INTO book (title) VALUES (?)`;
-const bookTitle = `JavaScript Primer 迷わないための入門`;
-const selectSql = `SELECT * FROM booka`;
-const dropTableSql = `DROP TABLE books`;
+const createTableSql =
+  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)";
+const insertSql = "INSERT INTO book (title) VALUES (?)";
+const bookTitle = "JavaScript Primer 迷わないための入門";
+const selectSql = "SELECT * FROM booka";
+const dropTableSql = "DROP TABLE books";
 
 runSql(db, createTableSql)
   .then(() => {
