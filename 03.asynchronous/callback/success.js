d5f2bf7f0b2a52b7a1343a81cdb9051ce314b53f
zecky1120 <takahide1120@gmail.com>
2025-03-09 15:36:27 +0900

    fix: テンプレートリテラルからダブルクォートに変更

--- 03.asynchronous/callback/success.js
+++ 03.asynchronous/callback/success.js
@@ -3,11 +3,12 @@
 import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
-const createTableSql = `CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)`;
-const insertSql = `INSERT INTO books (title) VALUES (?)`;
-const bookTitle = `JavaScript Primer 迷わないための入門`;
-const selectSql = `SELECT * FROM books`;
-const dropTableSql = `DROP TABLE books`;
+const createTableSql =
+  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT UNIQUE NOT NULL)";
+const insertSql = "INSERT INTO books (title) VALUES (?)";
+const bookTitle = "JavaScript Primer 迷わないための入門";
+const selectSql = "SELECT * FROM books";
+const dropTableSql = "DROP TABLE books";
 
 db.run(createTableSql, () => {
   console.log("テーブルが作成されました");
