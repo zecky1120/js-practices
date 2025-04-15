2df736704f1c196ac3f1f809e921b8f693c1416a
zecky1120 <takahide1120@gmail.com>
2025-04-15 21:21:38 +0900

    fix: callback/success.jsの出力する際の引数（rows）をrowに修正

--- 03.asynchronous/callback/success.js
+++ 03.asynchronous/callback/success.js
@@ -11,8 +11,8 @@ const dropTableSql = "DROP TABLE books";
 db.run(createTableSql, () => {
   db.run(insertSql, bookTitle, function () {
     console.log(`本(ID: ${this.lastID})が追加されました`);
-    db.get(selectSql, [this.lastID], (_, rows) => {
-      console.log(`ID: ${rows.id}, タイトル: ${rows.title}`);
+    db.get(selectSql, [this.lastID], (_, row) => {
+      console.log(`ID: ${row.id}, タイトル: ${row.title}`);
       db.run(dropTableSql, () => {
         console.log("テーブルを削除しました");
         db.close();
