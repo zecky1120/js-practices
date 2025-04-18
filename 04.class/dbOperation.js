6a0cfba1a7d6558cf56eac013b543c4a7e730c2a
zecky1120 <takahide1120@gmail.com>
2025-04-18 21:41:29 +0900

    add: dbOparationにコンストラクター追加

--- 04.class/dbOperation.js
+++ 04.class/dbOperation.js
@@ -21,40 +21,4 @@ export default class DbOperation {
       });
     };
   }
-
-  add = (content) => {
-    return new Promise((resolve, reject) => {
-      db.run(insertSql, content, (error) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      });
-    });
-  };
-
-  fetch = () => {
-    return new Promise((resolve, reject) => {
-      db.all(selectSql, (error, rows) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(rows);
-        }
-      });
-    });
-  };
-
-  remove = (content) => {
-    return new Promise((resolve, reject) => {
-      db.run(deleteSql, [content], (error) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      });
-    });
-  };
 }
