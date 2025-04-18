24fc611b9b82800be99215ba5de3beea1dd06e1a
zecky1120 <takahide1120@gmail.com>
2025-04-18 21:40:44 +0900

    add: dbOparationにエクスポートするクラス追加

--- 04.class/dbOperation.js
+++ 04.class/dbOperation.js
@@ -6,3 +6,55 @@ const createTableSql = `CREATE TABLE IF NOT EXISTS ${dbName} (id INTEGER PRIMARY
 const insertSql = `INSERT INTO ${dbName}(content) VALUES (?)`;
 const selectSql = `SELECT * FROM ${dbName}`;
 const deleteSql = `DELETE FROM ${dbName} WHERE content = ?`;
+
+export default class DbOperation {
+  constructor() {
+    this.createTable = () => {
+      return new Promise((resolve, reject) => {
+        db.exec(createTableSql, (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve();
+          }
+        });
+      });
+    };
+  }
+
+  add = (content) => {
+    return new Promise((resolve, reject) => {
+      db.run(insertSql, content, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  };
+
+  fetch = () => {
+    return new Promise((resolve, reject) => {
+      db.all(selectSql, (error, rows) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  };
+
+  remove = (content) => {
+    return new Promise((resolve, reject) => {
+      db.run(deleteSql, [content], (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  };
+}
