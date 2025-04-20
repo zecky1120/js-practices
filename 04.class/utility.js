74419e19ff88bcbb3fbc2f5721b5a5a3db490b34
zecky1120 <takahide1120@gmail.com>
2025-04-20 18:05:58 +0900

    add: Utilityクラス、コンストラクターの追加

--- 04.class/utility.js
+++ 04.class/utility.js
@@ -1,3 +1,10 @@
 import readline from "readline";
 import Select from "enquirer";
 import DbOperation from "./dbOperation.js";
+
+export default class Utility extends DbOperation {
+  constructor() {
+    super();
+    this.createTable();
+  }
+}
