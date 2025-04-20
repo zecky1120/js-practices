c786135ff87ba567705b48a1d5e77e9b6201ca05
zecky1120 <takahide1120@gmail.com>
2025-04-20 18:07:02 +0900

    add: #createMemoFromPromptの追加

--- 04.class/utility.js
+++ 04.class/utility.js
@@ -7,4 +7,24 @@ export default class Utility extends DbOperation {
     super();
     this.createTable();
   }
+  #createMemoFromPrompt() {
+    return new Promise((resolve) => {
+      const lines = [];
+      const reader = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+      reader.on("line", (line) => {
+        lines.push(line);
+      });
+      reader.on("close", () => {
+        if (lines[0].trim() === "") {
+          console.error("保存できません。1行目は必ず文字をいれてください。");
+        } else {
+          const memo = lines.join("\n");
+          resolve(memo);
+        }
+      });
+    });
+  }
 }
