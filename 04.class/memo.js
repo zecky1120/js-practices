5ac48e4ca5b8b42be15a180d381a2dc333c0a1dd
zecky1120 <takahide1120@gmail.com>
2025-04-20 18:13:37 +0900

    add: memo.jsにクラス、コンストラクターを追加

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -5,3 +5,9 @@ import Utility from "./utility.js";
 
 const utility = new Utility();
 const argv = minimist(process.argv.slice(1));
+
+class Memo {
+  constructor(argv) {
+    this.argv = argv;
+  }
+}
