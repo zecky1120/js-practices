5ac132b59859dc03a202de91e521bfd1107b3bdd
zecky1120 <takahide1120@gmail.com>
2025-04-27 20:33:48 +0900

    add: memoManagerにimport文を追加

--- 04.class/memoManager.js
+++ 04.class/memoManager.js
@@ -1,11 +1,10 @@
-import sqlite3 from "sqlite3";
-
-const db = new sqlite3.Database("./memo.db");
-const dbName = "memos";
-const createTableSql = `CREATE TABLE IF NOT EXISTS ${dbName} (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)`;
-const insertSql = `INSERT INTO ${dbName}(content) VALUES (?)`;
-const selectSql = `SELECT * FROM ${dbName}`;
-const deleteSql = `DELETE FROM ${dbName} WHERE content = ?`;
+import {
+  db,
+  createTableSql,
+  insertSql,
+  selectSql,
+  deleteSql,
+} from "./memoManagerVariable.js";
 
 export default class DbOperation {
   constructor() {
