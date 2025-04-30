43ab5255b60701259df86171c43dd94b04ab2a4d
zecky1120 <takahide1120@gmail.com>
2025-04-30 21:15:28 +0900

    fix: インスタンスプロパティ追加

--- 04.class/fileControl.js
+++ 04.class/fileControl.js
@@ -2,34 +2,44 @@ import sqlite3 from "sqlite3";
 
 export default class FileControl {
   constructor() {
-    this.createTable = () => {
-      return new Promise((resolve, reject) => {
-        db.exec(createTableSql, (error) => {
+    this.db = new sqlite3.Database("./memo.db");
+    this.dbName = "memos";
+  }
+
+  createTable = () => {
+    return new Promise((resolve, reject) => {
+      this.db.exec(
+        `CREATE TABLE IF NOT EXISTS ${this.dbName} (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)`,
+        (error) => {
           if (error) {
             reject(error);
           } else {
             resolve();
           }
-        });
-      });
-    };
-  }
+        },
+      );
+    });
+  };
 
   add = (content) => {
     return new Promise((resolve, reject) => {
-      db.run(insertSql, content, (error) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      });
+      this.db.run(
+        `INSERT INTO ${this.dbName}(content) VALUES (?)`,
+        content,
+        (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve();
+          }
+        },
+      );
     });
   };
 
   fetch = () => {
     return new Promise((resolve, reject) => {
-      db.all(selectSql, (error, rows) => {
+      this.db.all(`SELECT * FROM ${this.dbName}`, (error, rows) => {
         if (error) {
           reject(error);
         } else {
@@ -41,13 +51,17 @@ export default class FileControl {
 
   remove = (content) => {
     return new Promise((resolve, reject) => {
-      db.run(deleteSql, [content], (error) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      });
+      this.db.run(
+        `DELETE FROM ${this.dbName} WHERE content = ?`,
+        [content],
+        (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve();
+          }
+        },
+      );
     });
   };
 }
