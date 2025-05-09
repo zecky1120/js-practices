84181365f210b3f3708762ac50cf0b5d843b29c4
zecky1120 <takahide1120@gmail.com>
2025-05-09 21:11:49 +0900

    fix: asyncAwaitのerror.jsをtrycatchをラップする形にした

--- 03.asynchronous/asyncAwait/error.js
+++ 03.asynchronous/asyncAwait/error.js
@@ -9,29 +9,28 @@ const bookTitle = "JavaScript Primer 迷わないための入門";
 const selectSql = "SELECT * FROM booka WHERE id = ?";
 const dropTableSql = "DROP TABLE books";
 
-await run(db, createTableSql);
-console.log("テーブルが作成されました");
-
 try {
-  await run(db, insertSql, bookTitle);
-  console.log("本が追加されました");
-} catch (err) {
-  if (err instanceof Error && err.code === "SQLITE_ERROR") {
-    console.error(err.message);
-  } else {
-    throw err;
+  await run(db, createTableSql);
+  console.log("テーブルが作成されました");
+  try {
+    await run(db, insertSql, bookTitle);
+    console.log("本が追加されました");
+  } catch (err) {
+    if (err instanceof Error && err.code === "SQLITE_ERROR") {
+      console.error(err.message);
+    } else {
+      throw err;
+    }
   }
-}
-try {
-  await get(db, selectSql);
-} catch (err) {
-  if (err instanceof Error && err.code === "SQLITE_ERROR") {
-    console.error(err.message);
-  } else {
-    throw err;
+  try {
+    await get(db, selectSql);
+  } catch (err) {
+    if (err instanceof Error && err.code === "SQLITE_ERROR") {
+      console.error(err.message);
+    } else {
+      throw err;
+    }
   }
-}
-try {
   await run(db, dropTableSql);
   console.log("テーブルを削除しました");
 } finally {
